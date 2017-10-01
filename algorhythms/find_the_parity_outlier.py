f38d6597ce9f1ba1f2c0427e3fe04921056478c2
Gabriela Valle <gabrieladvmininel@gmail.com>
2017-10-01 19:45:54 +0100

    Removed commented code

--- algorhythms/find_the_parity_outlier.py
+++ algorhythms/find_the_parity_outlier.py
@@ -1,21 +1,6 @@
-# import unittest
-
-
 def find_outlier(integers):
     for o in integers:
         copied_list = integers.copy()
         copied_list.remove(o)
         if o % 2 != (copied_list[0] % 2) and (o % 2) != (copied_list[1] % 2):
             return o
-
-
-# class TestFindOutlier(unittest.TestCase):
-#
-#     def test_if_find_outlier_returns_different_number(self):
-#         self.assertEqual(find_outlier([2, 6, 8, 10, 3]), 3)
-#
-#
-# unittest.main()
-
-
-find_outlier([2, 6, 8, 10, 3])
