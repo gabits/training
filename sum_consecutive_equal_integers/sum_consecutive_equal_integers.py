5f6fb95ce5c8a557a7beda81797566bc6a19c78b
Gabriela Valle <gabrieladvmininel@gmail.com>
2017-10-01 20:06:15 +0100

    Added new exercise

---
+++ sum_consecutive_equal_integers/sum_consecutive_equal_integers.py
@@ -0,0 +1,27 @@
+"""You are given a list/array which contains only integers (positive and negative). Your job is to sum only the numbers
+that are the same and consecutive. The result should be one list.
+
+Extra credit if you solve it in one line. You can assume there is never an empty list/array and there will always be an
+integer.
+
+
+Notes:
+    - 1 == 1
+    - 1 != -1
+
+
+Examples:
+
+        [1,4,4,4,0,4,3,3,1]
+    Should return:
+        [1,12,0,4,6,1]
+
+         [1,1,7,7,3]
+    Should return:
+        [2,14,3]
+
+        [-5,-5,7,7,12,0]
+    Should return:
+        [-10,14,12,0]
+
+"""
