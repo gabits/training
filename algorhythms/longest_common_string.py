9e48fbcf98c1f3cc7ea8aa0e3dc6105d8fc53943
Gabriela Valle <gabrieladvmininel@gmail.com>
2017-11-07 20:57:22 +0000

    Adding new exercise from school

---
+++ algorhythms/longest_common_string.py
@@ -0,0 +1,14 @@
+"""Given strings s1 and s2, find the longest string s such that
+
+        s1 = s + u1 and s2 = s + u2,
+
+    e.g.
+        s1 = ’catch’
+        s2 = ‘cart’
+        u1 = ‘tch’
+        u2 = ‘rt’
+        comparison = ’t’ > ‘r’
+        order = s1 > s2
+"""
+
+# Is is to find the longest between both?
