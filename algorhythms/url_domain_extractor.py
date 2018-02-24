2e2aefcd4c14d34b7e4612624824b4e6987291a6
Gabriela Valle <gabrieladvmininel@gmail.com>
2018-02-24 16:48:02 +0000

    Adding url extractor exercise

---
+++ algorhythms/url_domain_extractor.py
@@ -0,0 +1,11 @@
+def domain_name(url):
+    url = [url[:i] for i in range(len(url) + 1) if url[i:i+4] == '.com'][0]
+    url = url.replace('.', '|') if '.' in url  else url.replace('//', '|').replace('/', '|')
+    if '|' in url:
+        url = [url[(len(url) - url[::-1].index(i)):] for i in url[::-1] if (i == '|')][0]
+    return url
+
+print(domain_name('www.github.com'))
+print(domain_name('http://google.com'))
+print(domain_name('gabi.com'))
+
